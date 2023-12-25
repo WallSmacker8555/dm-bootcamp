364a77583e6f8e785a13a88cb9ada09a1ebae9ff
WallSmacker8555 <rahul@DESKTOP-V3JSSBJ>
2023-12-25 16:28:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	        Your computer is under threat! Send 30 BTC NOW!!!
         </p>
         <a
           className="App-link"
