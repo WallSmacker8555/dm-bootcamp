9fd8d4cdf4fc940925cd456fa88ed7289a31228c
WallSmacker8555 <91920551+WallSmacker8555@users.noreply.github.com>
2023-12-26 17:21:12 -0600

    added CardViewer component base

---
+++ src/CardViewer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CardViewer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Card Viewer</h2>
+                <br/>
+                <button onClick={this.props.switchMode}>Go to Card Editor</button>
+            </div>
+        );
+    }
+}
+
+export default CardViewer;
