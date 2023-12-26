ab4fbb14c03490c7e72b8dae65be3fc87215f4c4
WallSmacker8555 <91920551+WallSmacker8555@users.noreply.github.com>
2023-12-26 17:22:07 -0600

    added conditional rendering for card editor and card viewer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CardEditor from "./CardEditor";
+import CardViewer from "./CardViewer";
 import React from 'react';
 
 class App extends React.Component {
@@ -9,14 +10,33 @@ class App extends React.Component {
       cards: [
         { front: 'front1', back: 'back1' },
         { front: 'front2', back: 'back2' }
-      ]
+      ],
+      editor: true,
     };
   }
 
+  addCard = card => {
+    const cards = this.state.cards.slice().concat(card);
+    this.setState({ cards });
+  };
+
+  deleteCard = index => {
+    const cards = this.state.cards.slice();
+    cards.splice(index, 1);
+  }
+
+  switchMode = () => this.setState({ editor: !this.state.editor });
+
   render() {
-    return (
-      <CardEditor cards={this.state.cards}/>
-    );
+    if (this.state.editor) {
+      return (
+        <CardEditor addCard={this.addCard} deleteCard={this.deleteCard} cards={this.state.cards} switchMode={this.switchMode}/>
+      );
+    } else {
+      return (
+        <CardViewer switchMode={this.switchMode}/>
+      );
+    }
   }
 
 }
