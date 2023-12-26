219baf60831a8c93f2539c496e5846ceecd91c98
WallSmacker8555 <91920551+WallSmacker8555@users.noreply.github.com>
2023-12-25 18:53:04 -0600

    got rid of goofy stuff and changed name of app to Flashcards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-	        Your computer is under threat! Send 30 BTC NOW!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">Testerino</div>
   );
 }
 
