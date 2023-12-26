92f3728ae30e000f18300059e3203ff6f1e5c71e
WallSmacker8555 <91920551+WallSmacker8555@users.noreply.github.com>
2023-12-26 15:17:44 -0600

    created list of cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App() {
-  return (
-    <div className="App">Testerino</div>
-  );
+import CardEditor from "./CardEditor";
+import React from 'react';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { front: 'front1', back: 'back1' },
+        { front: 'front2', back: 'back2' }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <CardEditor cards={this.state.cards}/>
+    );
+  }
+
 }
 
 export default App;
