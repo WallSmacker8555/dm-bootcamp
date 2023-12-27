4953ceed68cfac5e40665bea2420823424628d6b
WallSmacker8555 <91920551+WallSmacker8555@users.noreply.github.com>
2023-12-26 19:13:53 -0600

    added core functionality for CardViewer component

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import './CardViewer.css';
 
 class CardViewer extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { index: 0, displayFront: true};
+    };
+    
+    goBack = () => {
+        if (this.state.index === 0) {
+            return;
+        } else {
+            this.setState( { index : this.state.index - 1, displayFront: true} );
+        }
+    };
+
+    goForward = () => {
+        if (this.state.index === (this.props.cards.length - 1)) {
+            return;
+        } else {
+            this.setState( { index : this.state.index + 1, displayFront : true } );
+        }
+    };
+
+    flipCard = () => {
+        this.setState({displayFront : !this.state.displayFront});
+    };
+
     render() {
+
+        const card = this.props.cards[this.state.index][this.state.displayFront ? 'front' : 'back'];
+
         return (
             <div>
                 <h2>Card Viewer</h2>
+                <p>{this.state.index + 1}/{this.props.cards.length}</p>
+                <div className='card' onClick={this.flipCard}>
+                    {card}
+                </div>
+                <br/>
+                <button onClick={this.goBack}>Previous</button>
+                <button onClick={this.goForward}>Next</button>
                 <br/>
                 <button onClick={this.props.switchMode}>Go to Card Editor</button>
             </div>
